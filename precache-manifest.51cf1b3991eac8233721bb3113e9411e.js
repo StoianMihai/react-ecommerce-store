4ae6e69261e2d0b9587006e089ff2b5642f66e70
Stoian Mihai <mihai_stoian21@yahoo.com>
2019-11-21 13:55:56 +0200

    Updates

---
+++ precache-manifest.51cf1b3991eac8233721bb3113e9411e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cc7d8681223e1e7c1336399c581c35",
+    "url": "/react-ecommerce-store/index.html"
+  },
+  {
+    "revision": "f8ad1835e929359cca81",
+    "url": "/react-ecommerce-store/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bfde82a027e4be2be18a",
+    "url": "/react-ecommerce-store/static/css/main.b7b70262.chunk.css"
+  },
+  {
+    "revision": "f8ad1835e929359cca81",
+    "url": "/react-ecommerce-store/static/js/2.b86b9dfd.chunk.js"
+  },
+  {
+    "revision": "bfde82a027e4be2be18a",
+    "url": "/react-ecommerce-store/static/js/main.b074a5be.chunk.js"
+  },
+  {
+    "revision": "ae8e2011ddb8dfb328b8",
+    "url": "/react-ecommerce-store/static/js/runtime-main.8ead7bee.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-ecommerce-store/static/media/logo.9f7e506d.svg"
+  }
+]);
