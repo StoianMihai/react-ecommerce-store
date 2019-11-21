09a8b86a89e4b97bb72188f0a9ecdae151b34d4c
Stoian Mihai <mihai_stoian21@yahoo.com>
2019-11-21 14:25:37 +0200

    Updates

---
+++ precache-manifest.5459778ec2ad65fec5200b923c6b7a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2386d96377818b6656d74de0f60e5f29",
+    "url": "/react-ecommerce-store/index.html"
+  },
+  {
+    "revision": "f8ad1835e929359cca81",
+    "url": "/react-ecommerce-store/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "44df4861898795185b16",
+    "url": "/react-ecommerce-store/static/css/main.b7b70262.chunk.css"
+  },
+  {
+    "revision": "f8ad1835e929359cca81",
+    "url": "/react-ecommerce-store/static/js/2.b86b9dfd.chunk.js"
+  },
+  {
+    "revision": "44df4861898795185b16",
+    "url": "/react-ecommerce-store/static/js/main.75c45c20.chunk.js"
+  },
+  {
+    "revision": "ae8e2011ddb8dfb328b8",
+    "url": "/react-ecommerce-store/static/js/runtime-main.8ead7bee.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-ecommerce-store/static/media/logo.9f7e506d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edb2f1746be0adf6a403832cb3cdcc59.js"
+  "/react-ecommerce-store/precache-manifest.5459778ec2ad65fec5200b923c6b7a87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ecommerce-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
